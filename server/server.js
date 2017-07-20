381932202b5ce2044f073cd1ee0fe42d44500bda
Pei Ku <pei.ku@lmrc7734.rcoffice.ringcentral.com>
2017-07-20 14:12:09 -0700

    ic

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const express = require('express')
+const app = express();
+
+//
+const publicPath = path.join(__dirname, '../public')
+console.log(publicPath);
+
+const port = process.env.PORT || 3000 ;
+
+app.use(express.static(publicPath));
+
+//
+
+//
+app.listen(port , () => {
+  console.log(`started on port ${port}`)
+})
+
+module.exports = { app }
