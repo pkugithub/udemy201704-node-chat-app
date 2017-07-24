56000cb039c4d3b1199fcec95d581c42d09dd069
Pei Ku <pei.ku@lmrc7734.rcoffice.ringcentral.com>
2017-07-24 12:23:57 -0700

    chkpt - add socket connect and disconnect - server and client

--- server/server.js
+++ server/server.js
@@ -1,19 +1,31 @@
 const path = require('path')
 const express = require('express')
-const app = express();
+const socketIO = require('socket.io');
+const http = require('http');
 
 //
 const publicPath = path.join(__dirname, '../public')
 console.log(publicPath);
 
+const app = express();
 const port = process.env.PORT || 3000 ;
 
+var server = http.createServer(app) ;
+var io = socketIO(server) ;
+
+//
 app.use(express.static(publicPath));
 
-//
+io.on('connection', (socket) => {
+    console.log("user connection created ");
+
+    socket.on('disconnect', () => {
+      console.log('disconnected from client')
+    })
+});
 
 //
-app.listen(port , () => {
+server.listen(port , () => {
   console.log(`started on port ${port}`)
 })
 
